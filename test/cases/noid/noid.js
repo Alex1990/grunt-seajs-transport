08c9697bfa97a0a4fead088eb3d741bdf49384e6
Alex Chao <alexchao1990@gmail.com>
2014-09-23 08:24:23 +0800

    Add some test cases

---
+++ test/cases/noid/noid.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+    var $ = require("jQuery"),
+        ui = require("jQuery.UI");
+
+    $.fn.slider = function(){
+        alert("slider");
+    };
+});
---
+++ test/cases/prefix/prefix.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+    var $ = require("jquery");
+
+    $.fn.slider = function(elem, opts) {
+        alert("init");
+    };
+
+});
